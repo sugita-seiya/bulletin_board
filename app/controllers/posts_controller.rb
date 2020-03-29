ab4f8c6738a0888325adba4be4b8d8b8bdcd99ea
sugita-seiya <satreu16@ezweb.ne.jp>
2020-03-29 10:57:57 +0900

    スレッドにレス機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,19 @@ class PostsController < ApplicationController
 
   def show
     Post.find(params[:id])
-    @post = Post.new(params[:post].permit(:name, :body, :topic_id))
+    @post = Post.new(post_params)
     @post.save
     redirect_to topic_path(params[:post]['topic_id'])
   end
 
   def create
-    @post = Post.new(params[:post].permit(:topic_id, :name, :body))
+    @post = Post.new(post_params)
     @post.save
     redirect_to topic_path(params[:post]['topic_id'])
   end
 
+
+  def post_params
+    params.require(:post).permit(:name, :body, :topic_id)
+  end
 end
