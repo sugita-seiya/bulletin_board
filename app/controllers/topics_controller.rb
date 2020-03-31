72078428a09cb454692b35e106a54e7a683f777e
sugita-seiya <satreu16@ezweb.ne.jp>
2020-03-31 09:21:48 +0900

    formの書き方変更

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,18 +6,23 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:id])
-    @newpost = Post.new(:topic_id => params[:id])
+    @newpost = Post.new(topic_id: params[:id])
     @posts = Post.where(topic_id: params[:id])
   end
 
   def create
-    @topic = Topic.new(params[:topic].permit(:title))
+    @topic = Topic.new(topic_params)
     @topic.save
     redirect_to root_path
   end
 
   def search
     @topics =Topic.search(params[:keyword])
+  end
+
+  private
+  def topic_params
+    params.require(:topic).permit(:title)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :topic
 
+
+  validates :name,:body, presence: true
 end
